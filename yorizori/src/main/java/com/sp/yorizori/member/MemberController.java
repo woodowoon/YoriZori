cc0fc2fa7a501f299beec4e24a795084f56830d5
kimsan <kimsan@DESKTOP-33DR6F2>
2022-06-13 13:48:49 +0900

    김산 - 회원가입

--- yorizori/src/main/java/com/sp/yorizori/member/MemberController.java
+++ yorizori/src/main/java/com/sp/yorizori/member/MemberController.java
@@ -1,13 +1,24 @@
 package com.sp.yorizori.member;
 
+import java.util.HashMap;
+
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller("member.memberController")
 @RequestMapping(value = "/member/*")
@@ -15,6 +26,65 @@ public class MemberController {
 	
 	@Autowired
 	private MemberService service;
+	
+	@RequestMapping(value = "member", method = RequestMethod.GET)
+	public String memberForm(Model model) {
+		model.addAttribute("mode", "member");
+		return ".member.member";
+	}
+	
+	@RequestMapping(value = "addChef", method = RequestMethod.GET)
+	public String addChef(Model model) {
+		model.addAttribute("mode", "member");
+		return ".member.addChef";
+	}
+	
+	@RequestMapping(value = "member", method = RequestMethod.POST)
+	public String memberSubmit(Member dto,
+			final RedirectAttributes reAttr,
+			Model model) {
+
+		try {
+			
+			service.insertMember(dto);
+		} catch (DuplicateKeyException e) {
+			// 기본키 중복에 의한 제약 조건 위반
+			model.addAttribute("mode", "member");
+			model.addAttribute("message", "아이디 중복으로 회원가입이 실패했습니다.");
+			return ".member.member";
+		} catch (DataIntegrityViolationException e) {
+			// 데이터형식 오류, 참조키, NOT NULL 등의 제약조건 위반
+			model.addAttribute("mode", "member");
+			model.addAttribute("message", "제약 조건 위반으로 회원가입이 실패했습니다.");
+			return ".member.member";
+		} catch (Exception e) {
+			model.addAttribute("mode", "member");
+			model.addAttribute("message", "회원가입이 실패했습니다.");
+			return ".member.member";
+		}
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(dto.getNickName() + "님의 회원 가입이 정상적으로 처리되었습니다.<br>");
+		sb.append("메인화면으로 이동하여 로그인 하시기 바랍니다.<br>");
+
+		// 리다이렉트된 페이지에 값 넘기기
+		reAttr.addFlashAttribute("message", sb.toString());
+		reAttr.addFlashAttribute("title", "회원 가입");
+
+		return "redirect:/member/complete";
+	}
+	
+	@RequestMapping(value = "complete")
+	public String complete(@ModelAttribute("message") String message) throws Exception {
+
+		// 컴플릿 페이지(complete.jsp)의 출력되는 message와 title는 RedirectAttributes 값이다.
+		// F5를 눌러 새로 고침을 하면 null이 된다.
+
+		if (message == null || message.length() == 0) // F5를 누른 경우
+			return "redirect:/";
+
+		return ".member.complete";
+	}
 	
 	
 	@RequestMapping(value = "login", method = RequestMethod.GET)
@@ -63,5 +133,108 @@ public class MemberController {
 		session.invalidate();
 		
 		return "redirect:/";
+	}
+	
+	@RequestMapping(value = "pwd", method = RequestMethod.GET)
+	public String pwdForm(String dropout, Model model) {
+
+		if (dropout == null) {
+			model.addAttribute("mode", "update");
+		} else {
+			model.addAttribute("mode", "dropout");
+		}
+
+		return ".member.pwd";
+	}
+
+	@RequestMapping(value = "pwd", method = RequestMethod.POST)
+	public String pwdSubmit(@RequestParam String userPwd,
+			@RequestParam String mode, 
+			final RedirectAttributes reAttr,
+			HttpSession session,
+			Model model) {
+
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		Member dto = service.readMember(info.getUserId());
+		if (dto == null) {
+			session.invalidate();
+			return "redirect:/";
+		}
+
+		if (!dto.getUserPwd().equals(userPwd)) {
+			if (mode.equals("update")) {
+				model.addAttribute("mode", "update");
+			} else {
+				model.addAttribute("mode", "dropout");
+			}
+			model.addAttribute("message", "패스워드가 일치하지 않습니다.");
+			return ".member.pwd";
+		}
+
+		if (mode.equals("dropout")) {
+			// 게시판 테이블등 자료 삭제
+
+			// 회원탈퇴 처리
+			/*
+			 * Map<String, Object> map = new HashMap<>();
+			 * map.put("memberIdx", info.getMemberIdx());
+			 * map.put("userId", info.getUserId());
+			 */
+
+			// 세션 정보 삭제
+			session.removeAttribute("member");
+			session.invalidate();
+
+			StringBuilder sb = new StringBuilder();
+			sb.append(dto.getUserName() + "님의 회원 탈퇴 처리가 정상적으로 처리되었습니다.<br>");
+			sb.append("메인화면으로 이동 하시기 바랍니다.<br>");
+
+			reAttr.addFlashAttribute("title", "회원 탈퇴");
+			reAttr.addFlashAttribute("message", sb.toString());
+
+			return "redirect:/member/complete";
+		}
+
+		// 회원정보수정폼
+		model.addAttribute("dto", dto);
+		model.addAttribute("mode", "update");
+		return ".member.member";
+	}
+
+	@RequestMapping(value = "update", method = RequestMethod.POST)
+	public String updateSubmit(Member dto,
+			final RedirectAttributes reAttr,
+			Model model) {
+
+		try {
+			service.updateMember(dto);
+		} catch (Exception e) {
+		}
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(dto.getUserName() + "님의 회원정보가 정상적으로 변경되었습니다.<br>");
+		sb.append("메인화면으로 이동 하시기 바랍니다.<br>");
+
+		reAttr.addFlashAttribute("title", "회원 정보 수정");
+		reAttr.addFlashAttribute("message", sb.toString());
+
+		return "redirect:/member/complete";
+	}
+
+	// @ResponseBody : 자바 객체를 HTTP 응답 몸체로 전송(AJAX에서 JSON 전송 등에 사용)
+	@RequestMapping(value = "userIdCheck", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> idCheck(@RequestParam String userId) throws Exception {
+
+		String p = "true";
+		Member dto = service.readMember(userId);
+		if (dto != null) {
+			p = "false";
+		}
+
+		Map<String, Object> model = new HashMap<>();
+		model.put("passed", p);
+		return model;
 	}
 }
