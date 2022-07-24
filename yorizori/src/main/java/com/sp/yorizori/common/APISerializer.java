3e17193e0927417e466c48bd1dbf59f05f890b8b
dowoon <woodw920@gmail.com>
2022-07-24 21:08:25 +0900

    테스트

--- yorizori/src/main/java/com/sp/yorizori/common/APISerializer.java
+++ yorizori/src/main/java/com/sp/yorizori/common/APISerializer.java
@@ -11,7 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-@Service("common.apiSerializer")
+@Service("common.apiSerializer") 
 public class APISerializer {
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
